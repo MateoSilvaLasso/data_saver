a709281510bc7267e2795011436098b25ebc586d
Mateo Silva Lasso <mateosilva1901@gmail.com>
2025-05-19 12:27:15 -0500

    fix format

--- main.py
+++ main.py
@@ -7,19 +7,14 @@ app = FastAPI()
 
 
 class UserData(BaseModel):
-    nombre_completo: str
-    correo: str
-    celular: str
-    colegio: str
-    puntaje_icfes: str
-    estrato: str
+    info: str
 
 
 @app.post("/save_user_data")
 def save_user_data(user_data: UserData):
     try:
         saver = SaverOnline('TdG - I-Andy')
-        saver.guardar_datos(user_data.nombre_completo, user_data.correo, user_data.celular, user_data.colegio, user_data.puntaje_icfes, user_data.estrato)
+        saver.guardar_datos(user_data.info)
         return {"message": "Datos guardados correctamente en la base de datos."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
